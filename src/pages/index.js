754a654c848f97929813d5b4168b830cfddd6d1f
carlos ocejo <carlos.ocejo@encora.com>
2021-07-01 17:12:57 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const IndexPage = () => {
+  return (
+    <>
+    <main>
+      <nav>navbar</nav>
+      <section>sections 1</section>
+      <section>sections 2</section>
+      <section>sections 3</section>
+    </main>
+    <footer>
+      footer
+    </footer>
+    </>
+  )
+}
+
+export default IndexPage
